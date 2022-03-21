2443fb39bf7fce2ae4af455b485fafb552f7105b
Yuvaraj I <yuvarajyosef@gmail.com>
2022-03-21 19:17:26 +0530

    Test file added

---
+++ assignments/Day 10/HelloWorld.java
@@ -0,0 +1,13 @@
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+//		System.out = File("C:\\My Files\\Notes\\external.txt");
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
